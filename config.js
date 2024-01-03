b66e860fa865640aa53affa6102758b56e400765
QingYang Jewer <154724153+Jewer-9@users.noreply.github.com>
2024-01-03 16:57:22 +0000

    Update 8 files

---
+++ config.js
@@ -0,0 +1,87 @@
+/*
+ * https://rentry.org/teralomaniac_clewd
+ * https://github.com/teralomaniac/clewd
+ */
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+  Cookie: "SET YOUR COOKIE HERE",
+  CookieArray: [
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+  ],
+  Cookiecounter: 1,
+  CookieIndex: 0,
+  ProxyPassword: "Jiang24678055",
+  Ip: "127.0.0.1",
+  Port: 8444,
+  localtunnel: false,
+  BufferSize: 1,
+  SystemInterval: 3,
+  rProxy: "",
+  api_rProxy: "",
+  padtxt_placeholder: "",
+  PromptExperimentFirst: "",
+  PromptExperimentNext: "",
+  PersonalityFormat: "{{char}}'s personality: {{personality}}",
+  ScenarioFormat: "Dialogue scenario: {{scenario}}",
+  Settings: {
+    RenewAlways: true,
+    RetryRegenerate: false,
+    PromptExperiments: true,
+    SystemExperiments: true,
+    PreventImperson: false,
+    AllSamples: false,
+    NoSamples: false,
+    StripAssistant: false,
+    StripHuman: false,
+    PassParams: false,
+    ClearFlags: true,
+    PreserveChats: false,
+    LogMessages: true,
+    FullColon: true,
+    padtxt: 15000,
+    xmlPlot: true,
+    Superfetch: true,
+  },
+};
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
